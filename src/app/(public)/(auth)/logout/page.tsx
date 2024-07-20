650d3877853b9d9cb30239f882d8af00ac2ae6be
Huynh Nguyen <yugivip277@gmail.com>
2024-07-20 10:11:42 +0700

    Tự động logout khi access token hết hạn (trường hợp access token bị xóa khỏi cookie)

---
+++ src/app/(public)/(auth)/logout/page.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { getRefreshTokenFromLocalStorage } from '@/lib/utils'
+import { useLogoutMutation } from '@/queries/useAuth'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useEffect, useRef } from 'react'
+
+export default function LogoutPage() {
+  const { mutateAsync } = useLogoutMutation()
+  const router = useRouter()
+  const ref = useRef<any>(null)
+  const searchParams = useSearchParams()
+  const refreshTokenFromUrl = searchParams.get('refreshToken')
+  useEffect(() => {
+    if (
+      ref.current ||
+      refreshTokenFromUrl !== getRefreshTokenFromLocalStorage()
+    )
+      return
+    ref.current = mutateAsync
+    mutateAsync().then((res) => {
+      setTimeout(() => {
+        ref.current = null
+      }, 1000)
+      router.push('/login')
+    })
+  }, [mutateAsync, router, refreshTokenFromUrl])
+  return <div>Logout Page</div>
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -7,17 +7,33 @@ const unAuthPaths = ['/login']
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
-  const isAuth = Boolean(request.cookies.get('accessToken')?.value)
-  if (privatePaths.some((path) => pathname.startsWith(path)) && !isAuth) {
+  const accessToken = request.cookies.get('accessToken')?.value
+  const refreshToken = request.cookies.get('refreshToken')?.value
+
+  //Chưa đăng nhập thì không cho vào private path
+  if (privatePaths.some((path) => pathname.startsWith(path)) && !refreshToken) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
-  if (unAuthPaths.some((path) => pathname.startsWith(path)) && isAuth) {
+
+  //Đăng nhập rồi thì không cho vào login nữa
+  if (unAuthPaths.some((path) => pathname.startsWith(path)) && refreshToken) {
     return NextResponse.redirect(new URL('/', request.url))
+  }
+
+  //Trường hợp đăng nhập rồi, nhưng accessToken lại hết hạn
+  if (
+    privatePaths.some((path) => pathname.startsWith(path)) &&
+    !accessToken &&
+    refreshToken
+  ) {
+    const url = new URL('/logout', request.url)
+    url.searchParams.set('refreshToken', refreshToken)
+    return NextResponse.redirect(url)
   }
   return NextResponse.next()
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/manage/:path*', '/login'],
+  matcher: ['/manage/:path*', '/login']
 }
