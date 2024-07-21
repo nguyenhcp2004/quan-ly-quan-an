5b517650c7ebf5f379342840e61f396d028414de
Huynh Nguyen <yugivip277@gmail.com>
2024-07-21 09:55:39 +0700

    Xử lý trường hợp đang dùng thì access token hết hạn

--- src/components/app-provider.tsx
+++ src/components/app-provider.tsx
@@ -1,4 +1,5 @@
 'use client'
+import RefreshToken from '@/components/refresh-token'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
@@ -7,19 +8,20 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
-      refetchOnMount: false,
-    },
-  },
+      refetchOnMount: false
+    }
+  }
 })
 
 export default function AppProvider({
-  children,
+  children
 }: {
   children: React.ReactNode
 }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      <RefreshToken />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -45,3 +45,9 @@ export const getAccessTokenFromLocalStorage = () =>
   isBrowser ? localStorage.getItem('accessToken') : null
 export const getRefreshTokenFromLocalStorage = () =>
   isBrowser ? localStorage.getItem('refreshToken') : null
+
+export const setAccessTokenToLocalStorage = (value: string) =>
+  isBrowser && localStorage.setItem('accessToken', value)
+
+export const setRefreshTokenToLocalStorage = (value: string) =>
+  isBrowser && localStorage.setItem('refreshToken', value)
