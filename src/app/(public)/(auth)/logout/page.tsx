4d826f1158671e9801fadb165dfd34c019037f80
Huynh Nguyen <yugivip277@gmail.com>
2024-07-29 08:38:31 +0700

    Fix bug useSearchParam khi build

--- src/app/(public)/(auth)/logout/page.tsx
+++ src/app/(public)/(auth)/logout/page.tsx
@@ -7,9 +7,9 @@ import {
 } from '@/lib/utils'
 import { useLogoutMutation } from '@/queries/useAuth'
 import { useRouter, useSearchParams } from 'next/navigation'
-import { useEffect, useRef } from 'react'
+import { Suspense, useEffect, useRef } from 'react'
 
-export default function LogoutPage() {
+function Logout() {
   const { mutateAsync } = useLogoutMutation()
   const { setIsAuth } = useAppContext()
   const router = useRouter()
@@ -38,4 +38,11 @@ export default function LogoutPage() {
     }
   }, [mutateAsync, router, refreshTokenFromUrl, accessTokenFromUrl, setIsAuth])
   return <div>Logout ....</div>
+}
+export default function LogoutPage() {
+  return (
+    <Suspense>
+      <Logout />
+    </Suspense>
+  )
 }
