6369bc28df627d773a4dc620874fc3c6dfa1782b
Huynh Nguyen <yugivip277@gmail.com>
2024-07-31 21:35:28 +0700

    Xóa bàn ăn

--- src/app/manage/tables/table-table.tsx
+++ src/app/manage/tables/table-table.tsx
@@ -43,14 +43,15 @@ import {
   AlertDialogHeader,
   AlertDialogTitle
 } from '@/components/ui/alert-dialog'
-import { getVietnameseTableStatus } from '@/lib/utils'
+import { getVietnameseTableStatus, handleErrorApi } from '@/lib/utils'
 import { useSearchParams } from 'next/navigation'
 import AutoPagination from '@/components/auto-pagination'
 import { TableListResType } from '@/schemaValidations/table.schema'
 import EditTable from '@/app/manage/tables/edit-table'
 import AddTable from '@/app/manage/tables/add-table'
-import { useTableListQuery } from '@/queries/useTable'
+import { useDeleteTableMutation, useTableListQuery } from '@/queries/useTable'
 import { QRCodeTable } from '@/components/qrcode-table'
+import { toast } from '@/components/ui/use-toast'
 
 type TableItem = TableListResType['data'][0]
 
@@ -143,6 +144,22 @@ function AlertDialogDeleteTable({
   tableDelete: TableItem | null
   setTableDelete: (value: TableItem | null) => void
 }) {
+  const { mutateAsync } = useDeleteTableMutation()
+  const deleteTable = async () => {
+    if (tableDelete) {
+      try {
+        const result = await mutateAsync(tableDelete.number)
+        setTableDelete(null)
+        toast({
+          description: result?.payload.message
+        })
+      } catch (error) {
+        handleErrorApi({
+          error
+        })
+      }
+    }
+  }
   return (
     <AlertDialog
       open={Boolean(tableDelete)}
@@ -165,7 +182,7 @@ function AlertDialogDeleteTable({
         </AlertDialogHeader>
         <AlertDialogFooter>
           <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
+          <AlertDialogAction onClick={deleteTable}>Continue</AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
