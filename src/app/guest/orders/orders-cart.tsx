0e6618fb725fa986d2633c5febce8cbf8f7c56a5
Huynh Nguyen <yugivip277@gmail.com>
2024-08-03 14:51:09 +0700

    Tích hợp socket.io để lấy trạng thái order realtime

--- src/app/guest/orders/orders-cart.tsx
+++ src/app/guest/orders/orders-cart.tsx
@@ -1,13 +1,15 @@
 'use client'
 
 import { Badge } from '@/components/ui/badge'
+import socket from '@/lib/socket'
 import { formatCurrency, getVietnameseOrderStatus } from '@/lib/utils'
 import { useGuestGetOrderListQuery } from '@/queries/useGuest'
+import { UpdateOrderResType } from '@/schemaValidations/order.schema'
 import Image from 'next/image'
-import { useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
 
 export default function OrdersCart() {
-  const { data } = useGuestGetOrderListQuery()
+  const { data, refetch } = useGuestGetOrderListQuery()
   const orders = useMemo(() => data?.payload.data ?? [], [data])
 
   const totalPrice = useMemo(() => {
@@ -15,6 +17,33 @@ export default function OrdersCart() {
       return result + order.dishSnapshot.price * order.quantity
     }, 0)
   }, [orders])
+
+  useEffect(() => {
+    if (socket.connected) {
+      onConnect()
+    }
+
+    function onConnect() {
+      console.log(socket.id)
+    }
+
+    function onDisconnect() {
+      console.log('disconnect')
+    }
+
+    function onUpdateOrder(data: UpdateOrderResType['data']) {
+      refetch()
+    }
+    socket.on('update-order', onUpdateOrder)
+
+    socket.on('connect', onConnect)
+    socket.on('disconnect', onDisconnect)
+
+    return () => {
+      socket.off('connect', onConnect)
+      socket.off('disconnect', onDisconnect)
+    }
+  }, [refetch])
   return (
     <>
       {orders.map((order, index) => (
