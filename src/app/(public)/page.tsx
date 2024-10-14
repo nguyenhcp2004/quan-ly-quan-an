6c242eae4f1de265a1014e1c6183a7c60360031a
Huynh Nguyen <yugivip277@gmail.com>
2024-10-14 19:41:48 +0700

    Tích hợp i18n không routing

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -3,8 +3,10 @@ import { formatCurrency } from '@/lib/utils'
 import { DishListResType } from '@/schemaValidations/dish.schema'
 import Image from 'next/image'
 import Link from 'next/link'
+import { getTranslations } from 'next-intl/server'
 
 export default async function Home() {
+  const t = await getTranslations('HomePage')
   let dishList: DishListResType['data'] = []
   try {
     const result = await dishApiRequests.list()
@@ -29,7 +31,7 @@ export default async function Home() {
         />
         <div className='z-20 relative py-10 md:py-20 px-4 sm:px-10 md:px-20'>
           <h1 className='text-center text-xl sm:text-2xl md:text-4xl lg:text-5xl font-bold'>
-            Nhà hàng Big Boy
+            {t('title')}
           </h1>
           <p className='text-center text-sm sm:text-base mt-4'>
             Vị ngon, trọn khoảnh khắc
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { cn } from '@/lib/utils'
 import { Toaster } from '@/components/ui/toaster'
 import { ThemeProvider } from '@/components/theme-provider'
 import AppProvider from '@/components/app-provider'
+import { NextIntlClientProvider } from 'next-intl'
+import { getLocale, getMessages } from 'next-intl/server'
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -15,13 +17,18 @@ export const metadata: Metadata = {
   description: 'The best restaurant in the world'
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const locale = await getLocale()
+
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages()
   return (
-    <html lang='en' suppressHydrationWarning={true}>
+    <html lang={locale} suppressHydrationWarning={true}>
       <body
         className={cn(
           'min-h-screen bg-background font-sans antialiased',
