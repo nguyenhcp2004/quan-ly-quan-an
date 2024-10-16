c3b8615f31c52d6041d001d625562f6c8a628e54
Huynh Nguyen <yugivip277@gmail.com>
2024-10-16 22:08:59 +0700

    Thay đổi ngôn ngữ với i18n routing

--- src/components/switch-language.tsx
+++ src/components/switch-language.tsx
@@ -4,21 +4,33 @@ import {
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue
 } from '@/components/ui/select'
 import { Locale, locales } from '@/config'
 import { useLocale, useTranslations } from 'next-intl'
-import { setUserLocale } from '@/services/locale'
+import {
+  useParams,
+  usePathname,
+  useRouter,
+  useSearchParams
+} from 'next/navigation'
 export function SwitchLanguage() {
   const t = useTranslations('SwitchLanguage')
   const locale = useLocale()
+  const pathName = usePathname()
+  const params = useParams()
+  const searchParams = useSearchParams()
+  const router = useRouter()
   return (
     <Select
       value={locale}
       onValueChange={(value) => {
-        setUserLocale(value as Locale)
+        const locale = params.locale as Locale
+        const newPathName = pathName.replace(`/${locale}`, `/${value}`)
+        const fullUrl = `${newPathName}?${searchParams.toString()}`
+        router.push(fullUrl)
+        router.refresh()
       }}
     >
       <SelectTrigger className='w-[140px]'>
